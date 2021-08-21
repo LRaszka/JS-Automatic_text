d9d34f70a6964575d247cefd34addba862c55a24
LRaszka <Raszka.Lukas123@gmail.com>
2021-08-21 11:09:28 +0200

    Design done

---
+++ script.js
@@ -0,0 +1,15 @@
+const heading = document.getElementById("text");
+console.log(heading);
+
+const speedOriginal = document.getElementById("speed");
+console.log(speedOriginal);
+
+const text = "Lukáš vypisuje text";
+
+let idLetter = 1;
+let speed = 500 / speedOriginal.value;
+console.log(speed);
+
+function printText(){
+    
+}
